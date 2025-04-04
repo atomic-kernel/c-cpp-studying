8dcf99826d1f25c0b10785c3aa793a37c82968cb
atomic-kernel <qq570070308@gmail.com>
2025-04-04 14:40:04 +0800

    Update sem_bug.c

--- sem_bug.c
+++ sem_bug.c
@@ -14,28 +14,47 @@ union semun {
 				    (Linux-specific) */
 };
 
-int main()
-{
-	const int semid = semget(30, 1, IPC_EXCL | IPC_CREAT | 0666);
-	assert(semid >= 0);
-	union semun arg;
+int semid;
 
+static void child(void)
+{
+	// Notify the parent process that initialization has completed
+	struct sembuf sops;
+	sops.sem_num = 0;
+	sops.sem_op = 1;
+	sops.sem_flg = 0;
+	if (semop(semid, &sops, 1)) {
+		perror("child semop failed");
+		exit(-1);
+	}
+	
+	// wait for a kill
+	while (1) {}
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		printf("usage: ./test sem_key\n");
+		return 1;
+	}
+
+	const key_t key = atoi(argv[1]);
+	assert(key != 0);
+	semid = semget(key, 1, IPC_EXCL | IPC_CREAT | 0666);
+	assert(semid >= 0);
+
+	union semun arg;
 	arg.val = 0;
 	assert(semctl(semid, 0, SETVAL, arg) == 0);
 
 	while (1) {
 		pid_t pid = fork();
-		if (pid == 0) {
-			struct sembuf sops;
-			sops.sem_num = 0;
-			sops.sem_op = 1;
-			sops.sem_flg = 0;
-			if (semop(semid, &sops, 1)) {
-				perror("child semop failed");
-				exit(-1);
-			}
-			while (1) {}
-		}
+
+		if (pid == 0)
+			child();
+
+		// Wait for the child process to complete initialization
 		struct sembuf sops;
 		sops.sem_num = 0;
 		sops.sem_op = -1;
@@ -44,6 +63,8 @@ int main()
 			perror("semop failed");
 			exit(-1);
 		}
+
+		// kill child
 		assert(kill(pid, SIGKILL) == 0);
 		assert(waitpid(pid, NULL, 0) == pid);
 	}
