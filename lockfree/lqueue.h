39655dfa22d764dce7872aa174d85cc56a6119c1
root <root@DESKTOP-8TIG9K0.localdomain>
2025-07-28 22:14:31 +0800

    fix

--- lockfree/lqueue.h
+++ lockfree/lqueue.h
@@ -64,9 +64,8 @@ void lqueue_init(struct lqueue *const q)
 	q->last.raw_p = &q->dummy;
 }
 
-// Return whether lqueue is empty before queueing.
 static inline __attribute__((always_inline))
-bool lqueue_enqueue(struct lqueue *const q, struct lqueue_node *const node)
+void __lqueue_enqueue(struct lqueue *const q, struct lqueue_node *const node)
 {
 	size_t count;
 	struct tag_pnode old_last;
@@ -112,25 +111,25 @@ retry:
 	new_last.raw_p = node;
 	new_last.raw_count = count + 1;
 	atomic_compare_exchange_strong_explicit(&q->last.atomic, &old_last.raw, new_last.raw, memory_order_release, memory_order_relaxed);
+}
+
+// Return whether lqueue is empty before queueing.
+static inline __attribute__((always_inline))
+bool lqueue_enqueue(struct lqueue *const q, struct lqueue_node *const node)
+{
+	__lqueue_enqueue(q, node);
 
 	return !atomic_fetch_add_explicit(&q->num, 1, memory_order_release);
 }
 
 static inline __attribute__((always_inline))
-struct lqueue_node *lqueue_dequeue(struct lqueue *const q, bool *const is_empty_after_dequeue)
+struct lqueue_node *__lqueue_dequeue(struct lqueue *const q)
 {
-	size_t num;
 	struct tag_pnode old_first;
 	struct tag_pnode new_first;
 	struct tag_pnode old_last;
 	struct tag_pnode new_last;
 	struct lqueue_node *ret;
-
-	num = atomic_load_explicit(&q->num, memory_order_relaxed);
-	do {
-		if (!num)
-			return NULL;
-	} while (!atomic_compare_exchange_weak_explicit(&q->num, &num, num - 1, memory_order_acquire, memory_order_relaxed));
 
 	while (1) {
 		old_first.raw = atomic_load_explicit(&q->first.atomic, memory_order_acquire);
@@ -143,8 +142,6 @@ retry1:
 			struct tag_pnode old_next;
 
 			assert(old_first.raw_p == old_last.raw_p);
-			assert(old_last.raw_p != &q->dummy);
-			assert(q->dummy_is_free);
 
 			old_next.raw = atomic_load_explicit(&old_last.raw_p->next.atomic, memory_order_relaxed);
 			if (old_next.raw_count != old_last.raw_count) {
@@ -159,33 +156,53 @@ retry1:
 					old_last.raw = new_last.raw;
 				goto retry1;
 			}
-			q->dummy_is_free = 0;
-			lqueue_enqueue(q, &q->dummy);
-			continue;
+			return NULL;
 		}
 
 		new_first.raw_p = old_first.raw_p->next.raw_p;
 		new_first.raw_count = old_first.raw_count + 1;
 		if (!atomic_compare_exchange_weak_explicit(&q->first.atomic, &old_first.raw, new_first.raw, memory_order_release, memory_order_acquire))
 			goto retry0;
-		/* Success dequeue an element */
-		ret = old_first.raw_p;
-		struct tag_pnode tmp_old;
-		struct tag_pnode tmp_new;
-		tmp_old.raw_p = ret->next.raw_p;
-		tmp_old.raw_count = new_first.raw_count - 1;
-		tmp_new.raw_p = NULL;
-		tmp_new.raw_count = new_first.raw_count;
-		assert(atomic_compare_exchange_strong_explicit(&ret->next.atomic, &tmp_old.raw, tmp_new.raw, memory_order_relaxed, memory_order_relaxed));
-
-		if (ret != &q->dummy)
-			break;
-		assert(!q->dummy_is_free);
-		q->dummy_is_free = 1;
+		break;
 	}
+	/* Success dequeue an element */
+	ret = old_first.raw_p;
+	struct tag_pnode tmp_old;
+	struct tag_pnode tmp_new;
+	tmp_old.raw_p = ret->next.raw_p;
+	tmp_old.raw_count = new_first.raw_count - 1;
+	tmp_new.raw_p = NULL;
+	tmp_new.raw_count = new_first.raw_count;
+	assert(atomic_compare_exchange_strong_explicit(&ret->next.atomic, &tmp_old.raw, tmp_new.raw, memory_order_relaxed, memory_order_relaxed));
 
-	*is_empty_after_dequeue = (num == 1);
 	return ret;
 }
 
+static inline __attribute__((always_inline))
+struct lqueue_node *lqueue_dequeue(struct lqueue *const q, bool *const is_empty_after_dequeue)
+{
+	size_t num;
+	struct lqueue_node *ret;
+
+	num = atomic_load_explicit(&q->num, memory_order_relaxed);
+	do {
+		if (!num)
+			return NULL;
+	} while (!atomic_compare_exchange_weak_explicit(&q->num, &num, num - 1, memory_order_acquire, memory_order_relaxed));
+
+	while (1) {
+		ret = __lqueue_dequeue(q);
+		if (ret == &q->dummy) {
+			assert(!q->dummy_is_free);
+			q->dummy_is_free = 1;
+			continue;
+		} else if (ret)
+			break;
+		assert(q->dummy_is_free);
+		q->dummy_is_free = 0;
+		__lqueue_enqueue(q, &q->dummy);
+	}
+	*is_empty_after_dequeue = (num == 1);
+	return ret;
+}
 #endif
